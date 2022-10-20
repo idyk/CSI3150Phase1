b3cf51ce547b04da5319d91975e01097b2facf92
ian <dykas@oakland.edu>
2022-10-20 16:21:16 -0400

    tips progress bar added, more fun in issues

--- js/issues.js
+++ js/issues.js
@@ -3,8 +3,8 @@ const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
 const btn4 = document.getElementById("btn4");
-const btn5 = document.getElementById("btn5");
-const btn6 = document.getElementById("btn6");
+// const btn5 = document.getElementById("btn5");
+// const btn6 = document.getElementById("btn6");
 const issue = document.getElementById("issueGroup");
 const issueTitle = document.getElementById("issueGroupTitle");
 const issueImg = document.getElementById("issueImg");
@@ -17,13 +17,15 @@ let issueIndex = 0;
 let dogPets = 0;
 let catPets = 0;
 
+//check for mobile for some possible use cases for accesibility reasons
 const mobileCheck = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-
 console.log("On mobile? " + mobileCheck);
 
-//meme
+//meme declarations
 let clickCount = 0;
 const mistakesWereMade = new Audio("../media/audio/OH.mp3");
+const youDidIt = new Audio("../media/audio/goodJob.wav");
+youDidIt.loop = true;
 
 //set default issue to 1 on page load
 document.addEventListener("DOMContentLoaded", buttonOneShow);
@@ -32,7 +34,7 @@ document.addEventListener("DOMContentLoaded", buttonOneShow);
 let issueContent = [
   {
     title: "Exploding Computers",
-    image: "../media/images/favicon.png",
+    image: "../media/images/computer.jpg",
     description:
       "So this has never happened, but run far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far far away if it does!",
   },
@@ -43,25 +45,27 @@ let issueContent = [
       "WIRES ARE FUNNNNNNNNNNNNNNN!!!! (do NOT hit the Image 3 button 10 times!)",
   },
   {
-    title: "Ratty",
+    title: "Impending Doom: Ratty",
     image: "../media/images/RATTY.png",
-    description: "Ratty.",
+    description:
+      "Pet the Ratty. This is an important issue that you need to resolve right now. Move mouse around image on PC, tap image on mobile.",
   },
   {
-    title: "Gizmo",
+    title: "Help The Gizmo",
     image: "../media/images/gizmo.png",
-    description: "Gizmo.",
+    description:
+      "Pet the Gizmo. This is an important issue that you need to resolve right now. Move mouse around image on PC, tap image on mobile.",
   },
-  {
-    title: "5",
-    image: "../media/images/itsok_1.jpg",
-    description: "555555",
-  },
-  {
-    title: "6",
-    image: "../media/images/itsok_1.jpg",
-    description: "66666666",
-  },
+  // {
+  //   title: "5",
+  //   image: "../media/images/itsok_1.jpg",
+  //   description: "555555",
+  // },
+  // {
+  //   title: "6",
+  //   image: "../media/images/itsok_1.jpg",
+  //   description: "66666666",
+  // },
 ];
 
 //event listeners for button clicks
@@ -72,13 +76,15 @@ imgBtn2.addEventListener("click", networkImg2);
 imgBtn3.addEventListener("click", networkImg3);
 btn3.addEventListener("click", buttonThreeShow);
 btn4.addEventListener("click", buttonFourShow);
-btn5.addEventListener("click", buttonFiveShow);
-btn6.addEventListener("click", buttonSixShow);
+// btn5.addEventListener("click", buttonFiveShow);
+// btn6.addEventListener("click", buttonSixShow);
 
 if (mobileCheck == false) {
   issueImg.addEventListener("mousemove", petTheDog);
+  issueImg.addEventListener("mousemove", petTheCat);
 } else {
   issueImg.addEventListener("click", petTheDog);
+  issueImg.addEventListener("click", petTheCat);
 }
 
 //functions for each button
@@ -86,12 +92,13 @@ function buttonOneShow() {
   issueIndex = 0;
   disableImgButtons();
   mistakesWereMade.muted = true;
+  youDidIt.muted = true;
   innerBtns.style.opacity = 0;
   issue.classList.remove("issueTwo");
   issue.classList.remove("issueThree");
   issue.classList.remove("issueFour");
-  issue.classList.remove("issueFive");
-  issue.classList.remove("issueSix");
+  // issue.classList.remove("issueFive");
+  // issue.classList.remove("issueSix");
   issue.classList.add("issueOne");
   console.log(issueContent[0].title);
   console.log(issueContent[0].image);
@@ -105,11 +112,13 @@ function buttonTwoShow() {
   issueIndex = 1;
   enableImgButtons();
   innerBtns.style.opacity = 1;
+  mistakesWereMade.muted = true;
+  youDidIt.muted = true;
   issue.classList.remove("issueOne");
   issue.classList.remove("issueThree");
   issue.classList.remove("issueFour");
-  issue.classList.remove("issueFive");
-  issue.classList.remove("issueSix");
+  // issue.classList.remove("issueFive");
+  // issue.classList.remove("issueSix");
   issue.classList.add("issueTwo");
   console.log("Issue #2");
   console.log(issueContent[1].title);
@@ -125,12 +134,13 @@ function buttonThreeShow() {
   dogPets = 0;
   disableImgButtons();
   mistakesWereMade.muted = true;
+  youDidIt.muted = true;
   innerBtns.style.opacity = 0;
   issue.classList.remove("issueOne");
   issue.classList.remove("issueTwo");
   issue.classList.remove("issueFour");
-  issue.classList.remove("issueFive");
-  issue.classList.remove("issueSix");
+  // issue.classList.remove("issueFive");
+  // issue.classList.remove("issueSix");
   issue.classList.add("issueThree");
   console.log("Issue #3");
   console.log(issueContent[2].title);
@@ -143,14 +153,16 @@ function buttonThreeShow() {
 
 function buttonFourShow() {
   issueIndex = 3;
+  catPets = 0;
   disableImgButtons();
   mistakesWereMade.muted = true;
+  youDidIt.muted = true;
   innerBtns.style.opacity = 0;
   issue.classList.remove("issueOne");
   issue.classList.remove("issueTwo");
   issue.classList.remove("issueThree");
-  issue.classList.remove("issueFive");
-  issue.classList.remove("issueSix");
+  // issue.classList.remove("issueFive");
+  // issue.classList.remove("issueSix");
   issue.classList.add("issueFour");
   console.log("Issue #4");
   console.log(issueContent[3].title);
@@ -161,45 +173,45 @@ function buttonFourShow() {
   issueDesc.innerHTML = issueContent[3].description;
 }
 
-function buttonFiveShow() {
-  issueIndex = 4;
-  disableImgButtons();
-  mistakesWereMade.muted = true;
-  innerBtns.style.opacity = 0;
-  issue.classList.remove("issueOne");
-  issue.classList.remove("issueTwo");
-  issue.classList.remove("issueThree");
-  issue.classList.remove("issueFour");
-  issue.classList.remove("issueSix");
-  issue.classList.add("issueFive");
-  console.log("Issue #5");
-  console.log(issueContent[4].title);
-  console.log(issueContent[4].image);
-  console.log(issueContent[4].description);
-  issueTitle.innerHTML = issueContent[4].title;
-  issueImg.src = issueContent[4].image;
-  issueDesc.innerHTML = issueContent[4].description;
-}
+// function buttonFiveShow() {
+//   issueIndex = 4;
+//   disableImgButtons();
+//   mistakesWereMade.muted = true;
+//   innerBtns.style.opacity = 0;
+//   issue.classList.remove("issueOne");
+//   issue.classList.remove("issueTwo");
+//   issue.classList.remove("issueThree");
+//   issue.classList.remove("issueFour");
+//   issue.classList.remove("issueSix");
+//   issue.classList.add("issueFive");
+//   console.log("Issue #5");
+//   console.log(issueContent[4].title);
+//   console.log(issueContent[4].image);
+//   console.log(issueContent[4].description);
+//   issueTitle.innerHTML = issueContent[4].title;
+//   issueImg.src = issueContent[4].image;
+//   issueDesc.innerHTML = issueContent[4].description;
+// }
 
-function buttonSixShow() {
-  issueIndex = 5;
-  disableImgButtons();
-  mistakesWereMade.muted = true;
-  innerBtns.style.opacity = 0;
-  issue.classList.remove("issueOne");
-  issue.classList.remove("issueTwo");
-  issue.classList.remove("issueThree");
-  issue.classList.remove("issueFour");
-  issue.classList.remove("issueFive");
-  issue.classList.add("issueSix");
-  console.log("Issue #6");
-  console.log(issueContent[5].title);
-  console.log(issueContent[5].image);
-  console.log(issueContent[5].description);
-  issueTitle.innerHTML = issueContent[5].title;
-  issueImg.src = issueContent[5].image;
-  issueDesc.innerHTML = issueContent[5].description;
-}
+// function buttonSixShow() {
+//   issueIndex = 5;
+//   disableImgButtons();
+//   mistakesWereMade.muted = true;
+//   innerBtns.style.opacity = 0;
+//   issue.classList.remove("issueOne");
+//   issue.classList.remove("issueTwo");
+//   issue.classList.remove("issueThree");
+//   issue.classList.remove("issueFour");
+//   issue.classList.remove("issueFive");
+//   issue.classList.add("issueSix");
+//   console.log("Issue #6");
+//   console.log(issueContent[5].title);
+//   console.log(issueContent[5].image);
+//   console.log(issueContent[5].description);
+//   issueTitle.innerHTML = issueContent[5].title;
+//   issueImg.src = issueContent[5].image;
+//   issueDesc.innerHTML = issueContent[5].description;
+// }
 
 function networkImg1() {
   console.log("image 1");
@@ -219,8 +231,8 @@ function networkImg3() {
     issueImg.src = "../media/images/why.png";
     issueDesc.innerHTML = "Button privileges REVOKED!";
     mistakesWereMade.currentTime = 0;
-    mistakesWereMade.play();
     mistakesWereMade.muted = false;
+    mistakesWereMade.play();
     innerBtns.style.opacity = 0;
     clickCount = 0;
   } else {
@@ -250,12 +262,43 @@ function petTheDog() {
     if (mobileCheck == false) {
       if (dogPets == 999) {
         console.log("the dog is pet enough");
-        issueImg.src = "../media/images/gizmo.png";
+        issueImg.src = "../media/images/RATTY_PET.png";
+        issueDesc.innerHTML = "You did it!!!";
+        youDidIt.muted = false;
+
+        youDidIt.play();
       }
     } else {
       if (dogPets == 15) {
         console.log("the dog is pet enough");
-        issueImg.src = "../media/images/gizmo.png";
+        issueImg.src = "../media/images/RATTY_PET.png";
+        issueDesc.innerHTML = "You did it!!!";
+        youDidIt.muted = false;
+        youDidIt.play();
+      }
+    }
+  }
+}
+
+function petTheCat() {
+  if (issueIndex == 3) {
+    console.log("petting the cat");
+    catPets++;
+    if (mobileCheck == false) {
+      if (catPets == 999) {
+        console.log("the cat is pet enough");
+        issueImg.src = "../media/images/gizmo_pet.png";
+        issueDesc.innerHTML = "You did it!!!";
+        youDidIt.muted = false;
+        youDidIt.play();
+      }
+    } else {
+      if (catPets == 15) {
+        console.log("the cat is pet enough");
+        issueImg.src = "../media/images/gizmo_pet.png";
+        issueDesc.innerHTML = "You did it!!!";
+        youDidIt.muted = false;
+        youDidIt.play();
       }
     }
   }
--- js/tips.js
+++ js/tips.js
@@ -29,13 +29,32 @@ document.getElementById("next").addEventListener("click", nextButton);
 //Event listener for Previous Tip.
 document.getElementById("previous").addEventListener("click", backButton);
 
+//Progress bar
+const progress1 = document.getElementById("progress1");
+const progress2 = document.getElementById("progress2");
+const progress3 = document.getElementById("progress3");
+const progress4 = document.getElementById("progress4");
+const progress5 = document.getElementById("progress5");
+const progress6 = document.getElementById("progress6");
+const progress7 = document.getElementById("progress7");
+const progress8 = document.getElementById("progress8");
+const progress9 = document.getElementById("progress9");
+const progress10 = document.getElementById("progress10");
+
+document.addEventListener(
+  "DOMContentLoaded",
+  setProgressBar(currentTipsListIndex)
+);
+
 //Function for next button. Checks for index of the tips list. If valid for the array's length, it will increase the array.
 //Otherwise, it will not let you proceed.
 function nextButton() {
   console.log("Current tipsList index is: " + currentTipsListIndex);
+
   if (currentTipsListIndex < tipsList.length - 1) {
     currentTipsListIndex += 1;
     document.getElementById("tip").innerHTML = tipsList[currentTipsListIndex];
+    setProgressBar(currentTipsListIndex);
   } else {
     console.log("Max index reached");
   }
@@ -44,30 +63,66 @@ function nextButton() {
 //Function for back button. Same logic as the next button, but reversed to check for if the index is at 0 or not.
 function backButton() {
   console.log("Current tipsList index is: " + currentTipsListIndex);
+
   if (currentTipsListIndex > 0) {
     currentTipsListIndex -= 1;
+    setProgressBar(currentTipsListIndex);
     document.getElementById("tip").innerHTML = tipsList[currentTipsListIndex];
   } else {
     console.log("Min index reached");
   }
 }
 
-// while (1) {
-//   setTimeout(flash, 5);
-// }
-
-function flash() {
-  setTimeout(buttBlink, 3000);
-  setTimeout(buttBlinkAgain, 3000);
-  flash();
-}
-
-function buttBlink() {
-  document.getElementById("next").classList.remove("tipsButton");
-  document.getElementById("next").classList.add("tipsButtonBlink");
-}
-
-function buttBlinkAgain() {
-  document.getElementById("next").classList.remove("tipsButtonBlink");
-  document.getElementById("next").classList.add("tipsButton");
+function setProgressBar(index) {
+  console.log("setting bar to index " + index);
+  switch (index) {
+    case 0:
+      progress1.classList.add("tipOne");
+      progress2.classList.remove("tipTwo");
+      break;
+    case 1:
+      progress2.classList.add("tipTwo");
+      progress1.classList.remove("tipOne");
+      progress3.classList.remove("tipThree");
+      break;
+    case 2:
+      progress3.classList.add("tipThree");
+      progress4.classList.remove("tipFour");
+      progress2.classList.remove("tipTwo");
+      break;
+    case 3:
+      progress4.classList.add("tipFour");
+      progress5.classList.remove("tipFive");
+      progress3.classList.remove("tipThree");
+      break;
+    case 4:
+      progress5.classList.add("tipFive");
+      progress6.classList.remove("tipSix");
+      progress4.classList.remove("tipFour");
+      break;
+    case 5:
+      progress6.classList.add("tipSix");
+      progress5.classList.remove("tipFive");
+      progress7.classList.remove("tipSeven");
+      break;
+    case 6:
+      progress7.classList.add("tipSeven");
+      progress6.classList.remove("tipSix");
+      progress8.classList.remove("tipEight");
+      break;
+    case 7:
+      progress8.classList.add("tipEight");
+      progress7.classList.remove("tipSeven");
+      progress9.classList.remove("tipNine");
+      break;
+    case 8:
+      progress9.classList.add("tipNine");
+      progress10.classList.remove("tipTen");
+      progress8.classList.remove("tipEight");
+      break;
+    case 9:
+      progress10.classList.add("tipTen");
+      progress9.classList.remove("tipNine");
+      break;
+  }
 }
